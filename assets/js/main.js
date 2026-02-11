8f00ca0600ce51c5f306608640f6bf11f6929097
belld2307-collab <belld2307@gmail.com>
2026-02-11 04:31:28 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
+document.getElementById("year").textContent = new Date().getFullYear();
 
+const navBtn = document.getElementById("nav-btn");
+const nav = document.getElementById("nav");
+
+navBtn.addEventListener("click", () => {
+  nav.classList.toggle("open");
+});
